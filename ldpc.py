f2d5dafefdaf356cb9ef43974fdb19c023d1b353
Ryan Li <ryanli@Ryans-MacBook-Air.local>
2026-07-25 18:43:11 -0700

    update files

--- ldpc.py
+++ ldpc.py
@@ -1,1 +1,17 @@
+import numpy as np
+
+def gen_ldpc_matricies(k,m,col_weight=3,seed=None)
+    """
+Parameters:
+    k: # of message bits
+    n: # of parity check bits
+    col_weight : number of 1s per column of A (numpy array / matrix) --> col_weight check equations
+    seed: ensures randomness
+Returns:
+    A: (m x k) binary numpy array
+    H: (m x n) binary numpy array, parity check matrix
+    n: codeword length (m+k)
+    """
+
+    A = np.zeros((m,k), dtype = np.uint8)
 
