aa54fd1d2e13f65cd607d8e5f4569d5f79b6a475
Ryan <lryan9155@gmail.com>
2026-07-27 14:58:48 -0700

    unfinished rx.py

--- main_rx.py
+++ main_rx.py
@@ -21,7 +21,7 @@ sdr_rx = adi.Pluto("usb:0.1.5")
 rx = PlutoReceiver()
 rx.set_sdr(sdr_rx)
 rx.set_buffer_size(500e3)
-rx.set_channel(9)
+rx.set_channel(7)
 rx.set_gain_level(80)
 rx.desired_transmit_symbols_real = False
 rx.num_transmit_symbols = 200
