7298322fa1554664f942748d366e417402793016
Ryan <lryan9155@gmail.com>
2026-07-27 11:30:47 -0700

    Increase power level from 75 to 90

--- main_tx.py
+++ main_tx.py
@@ -20,7 +20,7 @@ sdr_tx = adi.Pluto("usb:1.1.5")
 tx = PlutoTransmitter()
 tx.set_sdr(sdr_tx)
 tx.set_channel(7)
-tx.set_power_level(75)
+tx.set_power_level(90)
 
 # ---------------------------------------------------------------
 # Generate random symbols.
