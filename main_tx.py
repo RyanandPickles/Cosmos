fcd1118cb7b9f4919a4534bfbe87081c068fbdb9
Ryan <lryan9155@gmail.com>
2026-07-27 13:41:16 -0700

    Update main_tx.py

--- main_tx.py
+++ main_tx.py
@@ -26,9 +26,8 @@ tx.set_power_level(95)
 # Generate random symbols.
 # ---------------------------------------------------------------
 num_pam_symbols = 200 # number of random data symbols to generate
-tx_symbols = 2*np.random.randint(0,2,num_pam_symbols) - 1
-tx_symbols = np.real(tx_symbols)
-
+M=16
+tx_symbols, const = gen_rand_qam_symbols(num_symbols, M=M)
 # ---------------------------------------------------------------
 # Transmit.
 # ---------------------------------------------------------------
