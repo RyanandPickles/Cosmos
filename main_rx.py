c1342aa72540e041079326ec5502e2864b707710
Ryan <lryan9155@gmail.com>
2026-07-27 16:39:27 -0700

    Update main_rx.py

--- main_rx.py
+++ main_rx.py
@@ -41,7 +41,7 @@ rx_symbols = rx.receive()
 
 rx_bits = qam_symbols_to_bits(rx_symbols, M, remainder)
 rx_bytes = bits_to_bytes(rx_bits)
-bytes_to_file(rx_bytes, "output/received.rtf")
+bytes_to_file(rx_bytes, "/Users/vincent/Cosmos/received.rtf")
 
 if True:
     plt.figure(figsize=(6, 6))
