df9da12ccc3d22f9783202255a1d57359df98449
Ryan <lryan9155@gmail.com>
2026-07-27 14:58:57 -0700

    Change transmission channel from 9 to 7

--- main_tx.py
+++ main_tx.py
@@ -20,7 +20,7 @@ sdr_tx = adi.Pluto("usb:1.1.5")
 
 tx = PlutoTransmitter()
 tx.set_sdr(sdr_tx)
-tx.set_channel(9)
+tx.set_channel(7)
 tx.set_power_level(95)
 
 # ---------------------------------------------------------------
