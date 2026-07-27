5593d92636131d14005527f4b8f7382e610988f2
Ryan <lryan9155@gmail.com>
2026-07-27 13:33:55 -0700

    Add files via upload

--- main_tx.py
+++ main_tx.py
@@ -15,19 +15,19 @@ dir_plots = 'plots/'
 # ---------------------------------------------------------------
 # Setup.
 # --------------------------------------3-------------------------
-sdr_tx = adi.Pluto("usb:1.1.5")
+sdr_tx = adi.Pluto("usb:1.12.5")
 
 tx = PlutoTransmitter()
 tx.set_sdr(sdr_tx)
-tx.set_channel(7)
-tx.set_power_level(90)
+tx.set_channel(9)
+tx.set_power_level(75)
 
 # ---------------------------------------------------------------
 # Generate random symbols.
 # ---------------------------------------------------------------
-M=16
-num_symbols = 200 # number of random data symbols to generate
-tx_symbols, const = gen_rand_qam_symbols(num_symbols, M=M)
+num_pam_symbols = 200 # number of random data symbols to generate
+tx_symbols = 2*np.random.randint(0,2,num_pam_symbols) - 1
+tx_symbols = np.real(tx_symbols)
 
 # ---------------------------------------------------------------
 # Transmit.
