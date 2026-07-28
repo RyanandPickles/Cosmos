f204af9fd366be69127f134844abf7f3d918b8b2
Ryan <lryan9155@gmail.com>
2026-07-28 10:33:47 -0700

    Update main_tx.py

--- main_tx.py
+++ main_tx.py
@@ -28,9 +28,14 @@ tx.set_power_level(95)
 # ---------------------------------------------------------------
 
 M=16
+k= int(np.log2(M))
+header_bits=32
+max_bits=262144
 
 filebytes = file_to_bytes("/Users/ryanli/Desktop/Test.txt")
 filebits = bytes_to_bits(filebytes)
+if len(filebits) > max_bits:
+    raise ValueError("file too big bud")
 
 tx_symbols, remainder = bits_to_qam_symbols(filebits, M)
 constellation = get_qam_constellation(M, Es=1)
