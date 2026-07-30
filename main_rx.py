f6d5fb8b7f9921361999b3689074bab228d5e625
vincentli08 <vincentli20081125@gmail.com>
2026-07-30 16:52:04 -0700

    Update main_rx.py

--- main_rx.py
+++ main_rx.py
@@ -17,7 +17,7 @@ OUTPUT_DIR = Path("/Users/vincent/Desktop/SDRReceivedLogs")
 PLUTO_URI = "usb:1.1.5"
 CHANNEL = 7
 GAIN_LEVEL = 80
-RX_BUFFER_SIZE = int(200e3)
+RX_BUFFER_SIZE = int(500e3)
 ROTATION_SECONDS = 60
 
 M = 16
