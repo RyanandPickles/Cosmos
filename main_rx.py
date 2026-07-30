ceb96b133f257ec25fbc090800e0bf20ae6004e0
vincentli08 <vincentli20081125@gmail.com>
2026-07-30 14:11:38 -0700

    Decrease gain level from 95 to 80

--- main_rx.py
+++ main_rx.py
@@ -16,7 +16,7 @@ import traceback
 OUTPUT_DIR = Path("/Users/vincent/Desktop/SDRReceivedLogs")
 PLUTO_URI = "usb:1.1.5"
 CHANNEL = 7
-GAIN_LEVEL = 95
+GAIN_LEVEL = 80
 RX_BUFFER_SIZE = int(1e6)
 ROTATION_SECONDS = 60
 
